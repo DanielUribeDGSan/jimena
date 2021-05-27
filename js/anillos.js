ac34e43bbc30f25169897b138ea9be8b74b9e624
Daniel Uribe <daniel.uribe.garcia07@gmail.com>
2021-05-26 20:21:32 -0500

    cambios home

---
+++ js/anillos.js
@@ -0,0 +1,12 @@
+cosnt = mostraranillosCrown = (img) => {
+  const img_id = document.getElementById('img__anillo');
+  if (img == 1) {
+    img_id.innerHTML = ` <img
+                src="./images/anillos/crown/imagen1.png"
+                data-zoom-image="./images/anillos/crown/imagen1.jpeg"
+                class="img-fluid zoom"
+                alt="jimena said"
+              />`;
+  } else if (img == 2) {
+  }
+};
